d8a0a22f88181c463c2f12726122e2dab896d3b0
SANA SHAHIN <40853082+sanashahin2225@users.noreply.github.com>
2022-04-12 16:09:12 +0530

    Update app.py

--- project1/app.py
+++ project1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "Hello from github runner...Hurray we did it."
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
